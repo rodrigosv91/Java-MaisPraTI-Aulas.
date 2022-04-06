465e49dcb6509efde96890568e7fad1afdaf522a
Rodrigo Vieira <rodrigosv91@gmail.com>
2022-04-06 03:05:02 -0300

    add: Extra/JPA_Hibernate

---
+++ Extra/JPA_Hibernate/src/main/java/com/mycompany/jpa_hibernate/model/Cliente.java
@@ -0,0 +1,44 @@
+
+package com.mycompany.jpa_hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Rodrigo
+ */
+@Entity 
+@Table(name = "cliente")
+public class Cliente {
+ 
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    //private int id;
+    //por segurança usar Integer 
+    //int nao aceita null e no banco de dados pode ser assinalado um campo de numeros como null
+    
+    @Column(name = "nome")
+    private String nome;
+    
+    //<editor-fold defaultstate="collapsed" desc="comment">    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    //</editor-fold>
+}
