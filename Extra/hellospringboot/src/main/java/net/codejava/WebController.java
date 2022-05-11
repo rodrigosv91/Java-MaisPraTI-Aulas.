f9ecfb7d7139327ef80f9c6e4a6a4d2eeb3a2a8b
Rodrigo Vieira <rodrigosv91@gmail.com>
2022-05-11 02:08:38 -0300

    edit: hello world spring boot

--- Extra/hellospringboot/src/main/java/net/codejava/WebController.java
+++ Extra/hellospringboot/src/main/java/net/codejava/WebController.java
@@ -2,14 +2,16 @@
 package net.codejava;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
     
     @RequestMapping("/hello")
-    public String sayHello(){
-        System.out.println("Saying hello...");
+    public String sayHello(Model model){
+        System.out.println("Greetings...");
+        model.addAttribute("message", "Welcome to Spring Boot Greetings");
         return "hello";
     }
     
